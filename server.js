4de2b14d836f4ead3771ca63907dfffb3483d73f
Dez <dez@deziddon.com>
2015-02-12 19:25:40 +0000

    Update server.js
    Creates / servers gzip where applicable.

--- server.js
+++ server.js
@@ -7,7 +7,16 @@
 ****************************************************/
 var port = process.env.PORT || process.env.port || 8081,
     staticFiles = require('node-static'),
-    fileServer = new staticFiles.Server('./public');
+    zlib = require('zlib'),
+    fs = require('fs'),
+    fileServer;
+
+
+// Setup static file server
+fileServer = new staticFiles.Server('./public', {
+    gzip:		true,
+    serverInfo:	'S'
+});
 
 
 // Listen for HTTP requests
@@ -24,6 +33,54 @@ require('http').createServer(function (req, res) {
         res.end('404');
     }
 }).listen(port);
+
+
+// Find and gzip static contents
+var gzipStaticContents = (function () {
+    var compressAsset = function (fileName) {
+        var input = fs.createReadStream(fileName),
+            output = fs.createWriteStream(fileName + '.gz');
+
+        input.pipe(zlib.createGzip()).pipe(output);
+    };
+
+    var compressStaticAssets = function (folder) {
+        var canCompress = ['css', 'html', 'htm', 'js', 'json', 'svg', 'txt'];
+
+        traverseFileSystem(folder, function (currentFile, currentPath) {
+            var fName = currentFile.split('.'),
+                extension = fName[fName.length - 1].toLowerCase();
+
+            if (fName && fName.length > 0 && extension) {
+                if (canCompress.indexOf(extension) > -1) {
+                    compressAsset(currentFile);
+                }
+            }
+        });
+    };
+
+    var traverseFileSystem = function (currentPath, func) {
+        var files = fs.readdirSync(currentPath),
+            currentFile, stats, key;
+
+        for (key in files) {
+            currentFile = currentPath + '/' + files[key];
+            stats = fs.statSync(currentFile);
+
+            if (stats.isFile()) {
+                if (func) {
+                    func(currentFile, currentPath)
+                }
+            } else if (stats.isDirectory()) {
+                traverseFileSystem(currentFile, func);
+            }
+        }
+    };
+
+    try {
+        compressStaticAssets('./public/');
+    } catch(ignore) {}
+} ());
 
 
 console.log('SiMpL3S now listening on port ' + port);
