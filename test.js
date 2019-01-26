3c5053605f4999b66557426e76f084939527b661
wtff0nzie <dez@backpagecms.net>
2019-01-26 16:26:11 +0000

    Add simple tests.

---
+++ test.js
@@ -0,0 +1,44 @@
+/***************************************************
+*                       SiMpL3S
+****************************************************
+*
+*   About:  Simple HTTP file flinger with a few
+*           perf tricks up it's sleeve.
+*
+****************************************************/
+'use strict';
+
+const simp = require('./lib/simpl3s');
+
+console.log('API', simp);
+console.log('Config (default)', simp.getConfig());
+console.log('Stats', simp.getStats());
+
+simp.setConfig({
+   path: './test'
+});
+console.log('Config (change path)', simp.getConfig());
+
+/*
+// Demonstrate stand alone warning
+
+simp.standAloneServer({
+    gzip    : true,
+    minify  : true
+});
+*/
+
+// Demonstrate single file serving
+require('http')
+    .createServer((req, res) => {
+        console.log(`Attempting to serve ${req.url}`);
+        simp.serveFile(req, res);
+    })
+    .listen(8080);
+
+console.log('Config (post run)', simp.getConfig());
+
+
+setInterval(() => {
+    console.log('Stats', simp.getStats());
+}, 10 * 1000);
